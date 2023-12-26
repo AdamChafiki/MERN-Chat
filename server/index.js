5044ff98401ca146d3ad33373db7a1e3c16cd912
Chafiki <adamchafiki3@gmail.com>
2023-12-26 11:35:42 +0100

    deploy

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const connectDb = require("./config/connDb");
 const { errorHandler, notFound } = require("./middlewares/error");
+const { log } = require("console");
 // Connect to mongoDb
 connectDb();
 
@@ -24,6 +26,29 @@ app.use("/api/auth", require("./routes/authRoute"));
 app.use("/api", require("./routes/userRoute"));
 app.use("/api", require("./routes/chatRoute"));
 app.use("/api", require("./routes/messageRoute"));
+
+// =======================deploy=======================================
+
+// Determine the main folder of the project
+const projectRoot = path.resolve(__dirname, ".."); // Assuming your main folder is one level up
+
+if (process.env.NODE_ENV === "production") {
+  // Serve static files from the "dist" directory
+  const staticPath = path.join(projectRoot, "client", "dist");
+  app.use(express.static(staticPath));
+
+  // Catch all other routes and serve the "index.html" file
+  const indexPath = path.resolve(projectRoot, "client", "dist", "index.html");
+  app.get("*", (req, res) => {
+    res.sendFile(indexPath);
+  });
+} else {
+  // In development, respond with "api working" for the root route
+  app.get("/", (req, res) => {
+    res.send("api working");
+  });
+}
+// =======================deploy=======================================
 
 //  Error handler middlware
 app.use(notFound);
@@ -64,8 +89,6 @@ io.on("connection", (socket) => {
       socket.in(user._id).emit("message recieved", newMessageRecieved);
     });
   });
-
- 
 
   socket.off("setup", () => {
     console.log("user disconnected");
